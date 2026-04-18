5a301ef397bf98914d0b72b9a005adab8c2a07b8
Logan Ouellette <lo2252a@american.edu>
2026-04-18 10:21:54 -0400

    Annotated aaa-global

--- R/aaa-globals.R
+++ R/aaa-globals.R
@@ -1,3 +1,11 @@
+#' Package-level imports and NSE global variable declarations
+#'
+#' Internal package annotations used to keep the generated namespace in sync and
+#' to silence `R CMD check` notes for non-standard evaluation in dplyr-powered
+#' code paths.
+#'
+#' @keywords internal
+#' @noRd
 #' @importFrom rlang .data
 #' @importFrom stats ave
 NULL
